cdf901cfddd7f692852ff3f53e4bf9b07b16e415
eacevedof <eacevedof@yahoo.es>
2017-11-11 21:38:35 +0100

    bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -7,14 +7,23 @@ require_once "vendor/autoload.php";
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
+//$sPathSrc = __DIR__."/entities";
+$sPathSrc = __DIR__."/mappings-annotations";
 $sPathSrc = __DIR__."/mappings-php";
+//$sPathSrc = __DIR__."/mappings-xml";
+//$sPathSrc = __DIR__."/mappings-yaml";
 $sPathSrc = realpath($sPathSrc);
 
+/**
+ * @type Doctrine\ORM\Configuration
+ */
 $config = Setup::createAnnotationMetadataConfiguration([$sPathSrc], $isDevMode);
 //$config = Setup::createAnnotationMetadataConfiguration([$sPathSrc], $isDevMode, null, null, false);
 // or if you prefer yaml or XML
-//$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
-//$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
+//$config = Setup::createXMLMetadataConfiguration([$sPathSrc], $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration([$sPathSrc], $isDevMode);
+
+//var_dump($config);die;
 
 $sPathDb =  __DIR__."/the_application/appdb/db_doctrine.sqlite3";
 $sPathDb = realpath($sPathDb);
