210ca26c4cca8768360f067c6b6b9d96f621e222
eacevedof <eacevedof@yahoo.es>
2017-11-12 15:18:42 +0100

    bootstrap.php Advanced

--- bootstrap.php
+++ bootstrap.php
@@ -1,40 +1,52 @@
 <?php
-// bootstrap.php
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
+// bootstrap.php Advanced
+//http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/advanced-configuration.html
 
 require_once "vendor/autoload.php";
 
+use Doctrine\ORM\EntityManager,
+    Doctrine\ORM\Configuration;
+
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
-$sPathSrc = __DIR__."/mappings-annotations";
-//$sPathSrc = __DIR__."/mappings-php";
-//$sPathSrc = __DIR__."/mappings-xml";
-//$sPathSrc = __DIR__."/mappings-yaml";
-//-----------------------------------------------
-//Primero se debe generar algún tipo de metadato 
-//rutas anteriores
-//-----------------------------------------------
-//$sPathSrc = __DIR__."/proxies";
-//$sPathSrc = __DIR__."/entities";
-//$sPathSrc = __DIR__."/repositories";
-$sPathSrc = realpath($sPathSrc);
 
-/**
- * @type Doctrine\ORM\Configuration
- */
-//$config = Setup::createAnnotationMetadataConfiguration([$sPathSrc], $isDevMode);
-$config = Setup::createAnnotationMetadataConfiguration([$sPathSrc], $isDevMode, null, null, false);
-//$config = Setup::createXMLMetadataConfiguration([$sPathSrc], $isDevMode);
-//$config = Setup::createYAMLMetadataConfiguration([$sPathSrc], $isDevMode);
+$arPaths = [
+    "mappings-annotations"=> realpath(__DIR__."/mappings-annotations"),
+    "mappings-php"=> realpath(__DIR__."/mappings-php"),
+    "mappings-xml"=> realpath(__DIR__."/mappings-xml"),
+    "mappings-yaml"=> realpath(__DIR__."/mappings-yaml"),
+    //-----------------------------------------------
+    //Primero se debe generar algún tipo de metadato 
+    //rutas anteriores
+    //-----------------------------------------------    
+    "proxies"=> realpath(__DIR__."/proxies"),
+    "entities"=> realpath(__DIR__."/entities"),
+    "repositories"=> realpath(__DIR__."/repositories"),
+];
 
-$sPathDb =  __DIR__."/the_application/appdb/db_doctrine.sqlite3";
-$sPathDb = realpath($sPathDb);
-// database configuration parameters
-$conn = array(
+if($applicationMode == "development"){
+    $cache = new \Doctrine\Common\Cache\ArrayCache;
+}else{
+    $cache = new \Doctrine\Common\Cache\ApcCache;
+}
+
+$config = new Configuration;
+$config->setMetadataCacheImpl($cache);
+$driverImpl = $config->newDefaultAnnotationDriver($arPaths["entities"]);
+$config->setMetadataDriverImpl($driverImpl);
+$config->setQueryCacheImpl($cache);
+$config->setProxyDir($arPaths["proxies"]);
+$config->setProxyNamespace("MyProject\Proxies");
+
+if($applicationMode == "development"){
+    $config->setAutoGenerateProxyClasses(true);
+}else{
+    $config->setAutoGenerateProxyClasses(false);
+}
+
+$connectionOptions = array(
     "driver" => "pdo_sqlite",
     "path" => $sPathDb,
 );
 
-// obtaining the entity manager
-$entityManager = EntityManager::create($conn, $config);
+$em = EntityManager::create($connectionOptions, $config);
