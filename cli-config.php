afd96e055ab0b576f037224ade3e487a30176e60
eacevedof <eacevedof@yahoo.es>
2017-11-12 15:34:46 +0100

    cli-config.php  Advanced Class Application not found in C:...\cli-config.php:7 el ejemplo no ha funcionado

--- cli-config.php
+++ cli-config.php
@@ -1,5 +1,11 @@
 <?php
-// cli-config.php
+// cli-config.php (advanced)
+// http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/advanced-configuration.html
+// 32.8. Setting up the Console
 require_once "bootstrap.php";
 
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
+$cli = new Application('Doctrine Command Line Interface', \Doctrine\ORM\Version::VERSION);
+$cli->setCatchExceptions(true);
+$cli->setHelperSet($helperSet);
+Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
+$cli->run();
