8ffd35cf872d927110a327b771ce2098c52e0da2
eacevedof <eacevedof@yahoo.es>
2017-11-12 16:19:21 +0100

    cli-config.php

--- cli-config.php
+++ cli-config.php
@@ -1,11 +1,5 @@
 <?php
-// cli-config.php (advanced)
-// http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/reference/advanced-configuration.html
-// 32.8. Setting up the Console
+// cli-config.php 
 require_once "bootstrap.php";
 
-$cli = new Application('Doctrine Command Line Interface', \Doctrine\ORM\Version::VERSION);
-$cli->setCatchExceptions(true);
-$cli->setHelperSet($helperSet);
-Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
-$cli->run();
+return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet($entityManager);
