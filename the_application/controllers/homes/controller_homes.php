1e38718565239ffa9cf535b7b5ef80b5125be1a4
eacevedof <eacevedof@yahoo.es>
2017-11-12 17:15:35 +0100

    Ejecuta ControllerHomes.index()

---
+++ the_application/controllers/homes/controller_homes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name TheApplication\Controllers\ControllerHomes
+ * @file controller_homes.php 
+ * @version 1.0.0
+ * @date 12-11-2017 17:01 (SPAIN)
+ * @observations:
+ * @requires  
+ */
+namespace TheApplication\Controllers;
+
+class ControllerHomes 
+{
+    public function __construct()
+    {
+        ;
+    }
+
+    public function index()
+    {
+        print_r("ControllerHomes.index()");
+    }
+
+}//ControllerHomes
--- the_public/index.php
+++ the_public/index.php
@@ -9,3 +9,8 @@ define("TFW_PATH_PROJECTDS",TFW_PATH_PROJECT.DIRECTORY_SEPARATOR);
 
 include_once(TFW_PATH_PROJECTDS."vendor/autoload.php");
 
+$sPathController = realpath(TFW_PATH_PROJECTDS."the_application/controllers/homes/controller_homes.php");
+include_once $sPathController;
+
+$oController = new TheApplication\Controllers\ControllerHomes();
+$oController->index();
