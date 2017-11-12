f23158130b8f7af1a5f44c6967b35d65a434d18d
eacevedof <eacevedof@yahoo.es>
2017-11-12 08:37:38 +0100

    la dejo como estaba

--- entities/AppActivity.php
+++ entities/AppActivity.php
@@ -1,18 +1,14 @@
 <?php
-// src/AppBundle/Entity/Product.php
-namespace AppBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
+
 
 /**
- * @ORM\Entity(repositoryClass="AppBundle\Repository\AppActivity")
+ * AppActivity
  */
 class AppActivity
 {
     /**
-    * @ORM\Id
-    * @ORM\Column(type="integer")
-    * @ORM\GeneratedValue(strategy="AUTO")
+    * @var Id
     */
     private $id;
 
