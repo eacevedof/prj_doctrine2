f4574fa1b8bdd17d19e9ea92956d06c15f791f19
eacevedof <eacevedof@yahoo.es>
2017-11-25 09:36:12 +0100

    backup bootstrap y cli-config

---
+++ bootstrap_orig..php
@@ -0,0 +1,43 @@
+<?php
+// bootstrap.php BASIC 01
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Annotations
+$isDevMode = true;
+
+$arPaths = [
+    "mappings-annotations"=> realpath(__DIR__."/mappings-annotations"),
+    "mappings-php"=> realpath(__DIR__."/mappings-php"),
+    "mappings-xml"=> realpath(__DIR__."/mappings-xml"),
+    "mappings-yaml"=> realpath(__DIR__."/mappings-yaml"),
+    //-----------------------------------------------
+    //Primero se debe generar algún tipo de metadato 
+    //rutas anteriores
+    //-----------------------------------------------    
+    "proxies"=> realpath(__DIR__."/proxies"),
+    "entities"=> realpath(__DIR__."/entities"),
+    "entities-bundle"=> realpath(__DIR__."/entities-bundle"),
+    "repositories"=> realpath(__DIR__."/repositories"),
+];
+/**
+ * @type Doctrine\ORM\Configuration
+ */
+//$config = Setup::createAnnotationMetadataConfiguration([$sPathSrc], $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration([$arPaths["mappings-annotations"]], $isDevMode, null, null, false);
+$config = Setup::createAnnotationMetadataConfiguration([$arPaths["entities-bundle"]], $isDevMode, null, null, false);
+//$config = Setup::createXMLMetadataConfiguration([$sPathSrc], $isDevMode);
+//$config = Setup::createYAMLMetadataConfiguration([$sPathSrc], $isDevMode);
+
+$sPathDb =  __DIR__."/the_application/appdb/db_doctrine.sqlite3";
+$sPathDb = realpath($sPathDb);
+// database configuration parameters
+$conn = array(
+    "driver" => "pdo_sqlite",
+    "path" => $sPathDb,
+);
+
+// obtaining the entity manager
+$entityManager = EntityManager::create($conn, $config);
