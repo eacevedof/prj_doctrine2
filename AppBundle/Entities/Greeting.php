943c5ad7795c2796cd75ff5ebfd8a31277a494ae
eacevedof <eacevedof@yahoo.es>
2018-08-10 14:35:05 +0200

    probando greeting en nuevo namespace

---
+++ AppBundle/Entities/Greeting.php
@@ -0,0 +1,52 @@
+<?php
+namespace AppBundle\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Greeting
+{
+
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $content;
+
+    public function __construct($content) {
+        $this->setContent($content);
+    }
+
+    /**
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent() {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent($content) {
+        $this->content = (string) $content;
+    }
+
+}
+   
